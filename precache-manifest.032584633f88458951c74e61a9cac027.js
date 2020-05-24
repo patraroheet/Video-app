ce23f544f7fa6816e48d4f08f4aa3f722eeef1a8
patraroheet <patraroheet@gmail.com>
2020-05-24 21:32:41 +0530

    Updates

---
+++ precache-manifest.032584633f88458951c74e61a9cac027.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dff985107022d4950e013817bdd80e16",
+    "url": "/Video-app/index.html"
+  },
+  {
+    "revision": "9b019d605f2cc09b5b38",
+    "url": "/Video-app/static/css/main.64e84079.chunk.css"
+  },
+  {
+    "revision": "21dbef25e590b5c96eb2",
+    "url": "/Video-app/static/js/2.3c9e2819.chunk.js"
+  },
+  {
+    "revision": "13b685b3db9ae92c2d07c213e5e3506f",
+    "url": "/Video-app/static/js/2.3c9e2819.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b019d605f2cc09b5b38",
+    "url": "/Video-app/static/js/main.e12d3198.chunk.js"
+  },
+  {
+    "revision": "fb66492d60a9e3f90caf",
+    "url": "/Video-app/static/js/runtime-main.626bf2a1.js"
+  }
+]);
